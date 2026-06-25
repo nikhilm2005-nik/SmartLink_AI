c7e71e1de2db246cdfb7b67bd7e2174f00572f0a
nikhilm2005-nik <nikhileducation2027@gmail.com>
2026-06-25 11:21:31 +0530

    create backend update controller to enable dynamic link routing

--- server/routes/url.routes.js
+++ server/routes/url.routes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-// Update the import to include getAnalytics
-const { shortenUrl, getUserUrls, deleteUrl, getAnalytics } = require('../controllers/url.controller');
+const { shortenUrl, getUserUrls, deleteUrl, getAnalytics, updateUrl } = require('../controllers/url.controller');
 const { protect } = require('../middleware/auth.middleware');
 
 router.post('/shorten', protect, shortenUrl);
 router.get('/my-urls', protect, getUserUrls);
 router.delete('/:id', protect, deleteUrl);
-// NEW: Analytics route
 router.get('/analytics', protect, getAnalytics);
+router.put('/:id', protect, updateUrl);
 
 module.exports = router;
